7d610f229d64711964150be48c545eb0425163d2
Bas <btsijtsma@gmail.com>
2016-02-04 21:41:18 +0100

    added routing structure

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var routes = require('require-dir');
+
+
+function(app) {
+  // require-dir reads the folder and require() all js and json files inside.
+  // calling require-dir without arguments will call from current dir
+  Object.keys(routes).forEach(function(routeName) {
+    var router = express.Router();
+    // initialize all routes with router arg to add functionality to the router.
+    require('./' + routeName)(router)
+    // Add router to the corresponding  route
+    app.use('/' + routeName, router)
+  })
+}
