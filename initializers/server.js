ebba3e33d6b087206992d8e786663b38604cb490
Bas <btsijtsma@gmail.com>
2016-02-04 21:49:31 +0100

    fixed server initialization

--- initializers/server.js
+++ initializers/server.js
@@ -3,14 +3,27 @@
 // })
 //
 // app.listen(3000)
+
+var PORTNR = 3000;
+
 var express = require('express');
 var bodyParser = require('body-parser')
 var app;
 
-var start = function(callback) {
+var startServer = function(callback) {
   var app = express();
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json({type: '*/*'}));
 
-  // add app.uses
-}
+  require('../routes/index.js')(app);
 
-module.exports = start;
+  app.listen(PORTNR);
+  console.log('listening on port 3000; http://bastronaut.com:3000/')
+
+  // called by async in app.js in case startServer fails
+  if (callback) {
+    return callback();
+  }
+};
+
+module.exports = startServer;
