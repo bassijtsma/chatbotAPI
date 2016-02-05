28182637bd8a421d455297a2b7cdd55099da1728
Bas <btsijtsma@gmail.com>
2016-02-05 15:06:55 +0100

    added get query to model

--- model/chatrule.js
+++ model/chatrule.js
@@ -1,7 +1,32 @@
-require('mongoose')
+var mongoose = require('mongoose');
+var mongodbPath = 'mongodb://localhost/chatbot'
 
-function getChatrules() {
+mongoose.connect(mongodbPath);
 
+/*
+var Schema = mongoose.Schema;
+var chatruleSchema = new Schema({
+  id : Number,
+  key : String,
+  response : String
+});
+
+var chatrule = mongoose.model('chatrule', Schema);
+*/
+
+
+
+function getChatrules(db, callback) {
+  var chatrules = [];
+  var cursor = db.collection('chatrule').find();
+
+  cursor.each(function (err, doc) {
+    if (doc != null) {
+      // put chatrule in array
+    } else {
+      //  callback with return array
+    }
+  });
 }
 
 function updateChatrule() {
